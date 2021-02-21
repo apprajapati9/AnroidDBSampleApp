d105c85d0d357dc83f417e4633b94b34dc571f64
apprajapati9 <apprajapati@hotmail.com>
2021-02-20 19:33:32 -0800

    databinding enabled

--- app/src/main/java/com/example/apprajapati/myshop/MainActivity.kt
+++ app/src/main/java/com/example/apprajapati/myshop/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.apprajapati.myshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.apprajapati.myshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
     }
 }
