f3a17924ce88e5a724c51255211aab7c326e428a
apprajapati9 <apprajapati@hotmail.com>
2021-02-20 22:42:46 -0800

    shopFragment added

---
+++ app/src/main/java/com/example/apprajapati/myshop/ShopFragment.kt
@@ -0,0 +1,31 @@
+package com.example.apprajapati.myshop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.apprajapati.myshop.databinding.ShopFragmentBinding
+
+class ShopFragment : Fragment() {
+
+    private lateinit var binding: ShopFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.shop_fragment, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
